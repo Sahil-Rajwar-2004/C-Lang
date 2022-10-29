47c2f70ed49da5961168977aa6c0aa9ffb72c968
Sahil Rajwar <justsahilrajwar2004@gmail.com>
2022-10-29 14:39:10 +0530

    Update calc.c

--- calc.c
+++ calc.c
@@ -1,26 +1,49 @@
 #include <stdio.h>
 
-int main(){
-	float x,y;
-	char oprator;
-	printf("num1: ");
-	scanf("%f",&x);
-	printf("num2: ");
-	scanf("%f",&y);	
-	printf("select operator (a,s,d,m): ");
-	scanf("%s",&oprator);
-	switch (oprator){
-		case 'a': printf("%f\n",x+y);
-				  break;
-		case 's': printf("%f\n",x-y);
-				  break;
-		case 'd': printf("%f\n",x/y);
-				  break;
-		case 'm': printf("%f\n",x*y);
-				  break;
-		default : printf("expected from (a,s,d,m)!");
-	}
+int summation(float x,float y);
+int subtraction(float x,float y);
+int multiplication(float x,float y);
+int division(float x,float y);
 
+int main(){
+	float a,b;
+	char opr;
+	printf("a = ");
+	scanf("%f",&a);
+	printf("b = ");
+	scanf("%f\n",&b);
+	scanf("%c",&opr);
+	
+	switch (opr){
+		case '+':printf("%f + %f = %d\n",a,b,summation(a,b));
+				 break;
+		case '-':printf("%f - %f = %d\n",a,b,subtraction(a,b));
+				 break;
+		case 'x':printf("%f x %f = %d\n",a,b,multiplication(a,b));
+				 break;
+		case '/':if (b != 0){
+					 printf("%f / %f = %d\n",a,b,division(a,b));
+				 }else {
+					 printf("Can't be divisible by Zero\n");
+				 }
+				 break;
+
+		default:
+				  printf("Invalid Operator!\n");
+				  break;
+	}
 	return 0;
 }
 
+int summation(float x,float y){
+	return x+y;
+}
+int subtraction(float x,float y){
+	return x-y;
+}
+int multiplication(float x,float y){
+	return x*y;
+}
+int division(float x,float y){
+	return x/y;
+}
