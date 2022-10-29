35ef8746461d691139f31e5d3e002bdb97bcb982
Sahil Rajwar <justsahilrajwar2004@gmail.com>
2022-10-29 09:59:14 +0530

    Add files via upload

---
+++ for_loop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	int f;
+	printf("frequency: ");
+	scanf("%d",&f);
+	for (int i=0;i<f;i++){
+		printf("Hello World!\n");
+	}
+	return 0;
+}
+
---
+++ while_loop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	int n;
+	printf("enter the number: ");
+	scanf("%d",&n);
+
+	int i = 0;
+	while (i<n){
+		printf("Hello World!\n");
+		i++;
+	}
+	return 0;
+}
+
